1bf7f0b9f8f9eb225239cb4de4507fefc947cab1
Laura Gusmao <laurargus@gmail.com>
2021-09-26 18:27:16 -0300

    Instala Material TextField e Material Icon, adicona Logo, Input Pesquisar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { Container, Search, Logo, Wrapper, Map } from './styles';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import logo from '../../assets/logo.svg';
 
-const Home = () => <Container>Hello World</Container>;
+const Home = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={ logo } alt="Logo do App" />
+          <TextField
+            label="Pesquisar"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search"/>}
+          >
+            <Input
+              value={ inputValue }
+              onChange={(e) => setInputValue(e.target.value) }
+            />
+          </TextField>
+        </Search>
+      </Container>
+      <Map>
+        oi
+      </Map>
+    </Wrapper>
+  );
+};
 
 export default Home;
