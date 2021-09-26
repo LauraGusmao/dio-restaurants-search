a9398e1ab20dc2e40cfd83e165ed03379e713e8e
Laura Gusmao <laurargus@gmail.com>
2021-09-26 19:18:12 -0300

    Instala font Roboto, React Slick, adiciona Carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
-import { Container, Search, Logo, Wrapper, Map } from './styles';
 import TextField, { Input } from '@material/react-text-field';
+import Slider from "react-slick";
 import MaterialIcon from '@material/react-material-icon';
+
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle, Carousel } from './styles';
+import { Card } from '../../components';
+
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
 
 const Home = () => {
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
 
   return (
     <Wrapper>
@@ -22,11 +36,17 @@ const Home = () => {
               onChange={(e) => setInputValue(e.target.value) }
             />
           </TextField>
+          <CarouselTitle>Na sua Área</CarouselTitle>
+          <Carousel { ...settings }>
+            <Card photo={ restaurante } title="Nome" />
+            <Card photo={ restaurante } title="Nome" />
+            <Card photo={ restaurante } title="Nome" />
+            <Card photo={ restaurante } title="Nome" />
+            <Card photo={ restaurante } title="Nome" />
+          </Carousel>
         </Search>
       </Container>
-      <Map>
-        oi
-      </Map>
+      <Map />
     </Wrapper>
   );
 };
