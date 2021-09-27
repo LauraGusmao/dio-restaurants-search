819f9f1739a0538fd5c242db6917d682069151a5
Laura Gusmao <laurargus@gmail.com>
2021-09-27 03:05:16 -0300

    Adiciona Skeleton nos RestautantCards

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import ReactStars from "react-rating-stars-component";
 
 import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles'
+import Skeleton from '../Skeleton';
 
 import restaurante from '../../assets/restaurante-fake.png';
 
 const RestaurantCard = ({ restaurant, onClick }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <Restaurant onClick={ onClick }>
       <RestaurantInfo>
@@ -19,7 +22,13 @@ const RestaurantCard = ({ restaurant, onClick }) => {
         />
         <Address>{ restaurant.vicinity || restaurant.formatted_address }</Address>
       </RestaurantInfo>
-      <RestaurantPhoto src={ restaurant.photos ? restaurant.photos[0].getUrl() : restaurante } alt="Foto do Restaurante" />
+      <RestaurantPhoto
+        imageLoaded={ imageLoaded }
+        src={ restaurant.photos ? restaurant.photos[0].getUrl() : restaurante }
+        alt="Foto do Restaurante"
+        onLoad={ () => setImageLoaded(true) }
+      />
+      { !imageLoaded && <Skeleton width="100px" height="100px" /> }
     </Restaurant>
   );
 };
